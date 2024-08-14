1b3b34bc3b3135aec53f20e49818e3f6841e161d
Hishita Gupta <159264060+HishitaGupta@users.noreply.github.com>
2024-08-14 18:12:47 +0530

    first commit

---
+++ ecommerce/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import ProductList from './features/product-list/ProductList'
+import Home from './features/pages/Home'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Home/>
+      
+    </>
+  )
+}
+
+export default App
